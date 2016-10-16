add629488f36e0b103642162bf7e29db2dfef18c
Brian Tom <briantom171@gmail.com>
2016-10-15 17:07:58 -0700

    backend start

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,86 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+
+
+router.get('/', function(req, res) {
+
+  // SOLUTION:
+  // =========
+  // use the Burger model to find all Burgers,
+  // and use the include option to grab info from the User model.
+  // This will let us show the Burger and it's owner.
+  models.D_Rank.findAll({
+    include: [ models.User ]
+  })
+  // connect the findAll to this .then
+  .then(function(D_Rank) {
+    // grab the user info from our req.
+    // How is it in our req?
+    // This info gets saved to req via the users_controller.js file.
+    res.render('burgers/index', {
+      user_id: req.session.user_id,
+      email: req.session.user_email,
+      logged_in: req.session.logged_in,
+      D_Rank: D_Rank
+    });
+  });
+});
+
+router.post('/create', function (req, res) {
+  // SOLUTION:
+  // =========
+  // use the Burger model to create a Burger based on what's
+  // passed in req.body (burger_name, devoured, user_id)
+  models.Burger.create({
+    burger_name: req.body.burger_name,
+    devoured: req.body.devoured,
+    user_id: req.session.user_id
+  })
+  // connect the .create to this .then
+  .then(function() {
+    res.redirect('/');
+  });
+});
+
+router.put('/update/:id', function(req,res) {
+  // SOLUTION:
+  // =========
+  // use the Burger model to update a Burger's devoured status
+  // based on the boolean passed in req.body devoured
+  // and the id of the Burger (as passed in the url)
+  models.Burger.update(
+  {
+    devoured: req.body.devoured
+  },
+  {
+    where: { id : req.params.id }
+  })
+  // connect it to this .then.
+  .then(function (result) {
+    res.redirect('/');
+  }, function(rejectedPromiseError){
+
+  });
+});
+
+
+router.delete('/delete/:id', function(req,res) {
+  // SOLUTION:
+  // =========
+  // use the Burger model to delete a Burger
+  // based on the id passed in the url
+  models.Burger.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  // connect it to this .then.
+  .then(function() {
+    res.redirect('/');
+  });
+
+});
+
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var D_Rank = sequelize.define("D_Rank", {
+    team: DataTypes.STRING,
+    PassYardsPerGame: DataTypes.FLOAT,
+    PassYardsPerGameRank: DataTypes.FLOAT,
+    RushYardsPerGame: DataTypes.FLOAT,
+    RushYardsPerGameRank: DataTypes.FLOAT,
+    PointsPerGame: DataTypes.FLOAT,
+    PointsPerGameRank: DataTypes.FLOAT,
+    AveragePassRank: DataTypes.FLOAT,
+    TruePassRank: DataTypes.FLOAT,
+    AverageRushRank: DataTypes.FLOAT,
+    TrueRushRank: DataTypes.FLOAT
+  }, {
+
+    underscored: true,
+
+    freezeTableName: true,
+
+    // define the table's name
+    tableName: 'D_Rank',
+
+    classMethods: {
+      associate: function(models) {
+        D_Rank.belongsTo(models.User, {
+          onDelete: "CASCADE",
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      }
+    }
+  });
+
+  return D_Rank;
+};
